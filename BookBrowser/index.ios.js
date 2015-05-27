acb5dd0885265c5c628d198d8297ba7d28166f14
bbofrk <mingliwanglau@gmail.com>
2015-05-28 07:59:26 +1200

    first page layout

--- BookBrowser/index.ios.js
+++ BookBrowser/index.ios.js
@@ -8,15 +8,54 @@ var React = require('react-native');
 var {
   AppRegistry,
   View,
+  Text,
+  StyleSheet,
+  TextInput,
 } = React;
 
 var BookBrowser = React.createClass({
   render: function() {
     return (
-      <View></View>
+      <View style={styles.container}>
+        <Text style={styles.headline}>BookBrowser</Text>
+        <Text style={styles.label}>Find books containing</Text>
+        <TextInput
+          style={styles.textInput}
+          placeholder="e.g. Javascript or Mobile"
+          />
+      </View>
     );
   }
 });
 
-
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#5AC8FA'
+  },
+  headline: {
+    fontSize: 36,
+    fontWeight: 'bold',
+    color: '#FFF',
+    marginBottom: 28
+  },
+  label: {
+    fontSize: 24,
+    fontWeight: 'normal',
+    color: '#FFF',
+    marginBottom: 8
+  },
+  textInput: {
+    borderWidth: 0.5,
+    borderColor: '#BEBE93',
+    backgroundColor: '#FFF',
+    height: 40,
+    marginLeft: 50,
+    marginRight: 50,
+    padding: 8
+  }
+});
 AppRegistry.registerComponent('BookBrowser', () => BookBrowser);
